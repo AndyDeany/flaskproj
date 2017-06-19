6d9b3e9021d4181ba803671f0aa83df684e459ef
Andrew Dean <oneandydean@hotmail.com>
2017-06-19 21:35:12 +0100

    Add simple main.py file for running the webapp and creating a user (kritti <3)

---
+++ main.py
@@ -0,0 +1,27 @@
+import flask
+
+from lib.user import User
+
+
+app = flask.Flask(__name__)
+
+
+User(username="svesar99",
+     forename="sara", surname="svedlund",
+     dob=(1999, 1, 15),
+     bio="The cutest kitty in all the land.",
+     quote="Meow")
+
+
+@app.route("/user/<username>")
+@app.route("/u/<username>")
+def profile(username):
+    user = User.find(username)
+    if user is None:
+        return "User '%s' not found." % username
+    else:
+        return flask.render_template("user.html", user=user)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
